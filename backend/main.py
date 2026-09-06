d4f98516f901a72b82f5788243acfecca559d3ca
zhao413870 <GoldenRipple@outlook.com>
2026-09-06 23:35:58 +0800

    完成anaiyze接口的实现

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from pypinyin import lazy_pinyin, Style
+from snownlp import SnowNLP
 
 app = FastAPI()
 
@@ -35,14 +37,23 @@ class AnalyzeRequest(BaseModel):
 @app.get("/api/profile")
 def get_profile():
     return profile
+def score_label(score):
+    if score >= 0.6:
+        return "偏积极"
+    elif score <= 0.4:
+        return "偏消极"
+    else:
+        return "中性"
 
 
 @app.post("/api/analyze")
 def analyze(req: AnalyzeRequest):
+    text=req.text
+    score = round(SnowNLP(text).sentiments, 2) 
     return {
         "text": req.text,
-        "score": 0.5,
-        "label": "偏平静",
-        "pinyin": "（模块 6 再说）",
+        "score": score,
+        "label": score_label(score),
+        "pinyin": " ".join(lazy_pinyin(text, style=Style.TONE)),
     }
 
